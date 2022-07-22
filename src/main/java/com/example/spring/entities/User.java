6ce91565837aa71906d6d85782b4031031daa430
Maurício <mauriciomendes994@gmail.com>
2022-07-22 18:00:54 -0300

    criando user

--- src/main/java/com/example/spring/entities/User.java
+++ src/main/java/com/example/spring/entities/User.java
@@ -2,9 +2,16 @@ package com.example.spring.entities;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class User implements Serializable{
     
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String email;
